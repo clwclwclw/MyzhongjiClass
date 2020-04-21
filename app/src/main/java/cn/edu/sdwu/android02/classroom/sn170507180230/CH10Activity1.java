e5964cceae1784140cffe6a68969adb149e7a32d
clwclwclw <729701285@qq.com>
2020-04-21 14:33:52 +0800

    reason

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180230/CH10Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180230/CH10Activity1.java
@@ -1,28 +1,59 @@
 package cn.edu.sdwu.android02.classroom.sn170507180230;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
+import android.view.View;
 public class CH10Activity1 extends AppCompatActivity {
-
+private Integer count;//点击按键的计数器
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(CH10Activity1.class.toString(),"onCreate");
+        setContentView(R.layout.layout_ch10_1);
+       count=0;
     }
 
 
+    public void  finishClick(View view){
+        finish();//关闭界面
+    }
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(CH10Activity1.class.toString(),"onStart");
+    }
+//计数的方法
+    public void counter(View view){
+        count++;
+        Log.i(CH10Activity1.class.toString(),"counter:"+count);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        //使用本方法保存一些界面的状态信息
+        //数据保存Bundle对象中
+        outState.putInt("counter",count);
+        super.onSaveInstanceState(outState);
+        Log.i(CH10Activity1.class.toString(),"savedInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count=savedInstanceState.getInt("counter");
+        Log.i(CH10Activity1.class.toString(),"onRestoreInstanceState");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(CH10Activity1.class.toString(),"onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
