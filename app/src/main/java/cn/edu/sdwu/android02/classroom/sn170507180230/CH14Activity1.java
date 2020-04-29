54dbfd451035ec1869346a5071c5b04a555121cf
clwclwclw <729701285@qq.com>
2020-04-29 20:42:24 +0800

    reason

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180230/CH14Activity1.java
@@ -0,0 +1,106 @@
+package cn.edu.sdwu.android02.classroom.sn170507180230;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class CH14Activity1 extends AppCompatActivity {
+    private MyOpenHelper myOpenHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.layout_ch14_1);
+
+            myOpenHelper=new MyOpenHelper(this);
+            //以可写方式打开数据库(如果数据库不存在，自动创建数据库)
+            SQLiteDatabase sqLiteDatabase=myOpenHelper.getWritableDatabase();
+
+            //使用完毕，将数据库关闭
+            sqLiteDatabase.close();
+        }
+
+    public  void insert(View view ){
+        //以可写方式打开数据库(如果数据库不存在，自动创建数据库)
+        SQLiteDatabase sqLiteDatabase=myOpenHelper.getWritableDatabase();
+        try{
+            //将插入的数据放置在ContentValues中
+            //事务的处理
+            sqLiteDatabase.beginTransaction();//开启事务
+            ContentValues contentValues=new ContentValues();
+            contentValues.put("stunema","Tom");
+            contentValues.put("stutel","15165082221");
+            sqLiteDatabase.insert("student",null,contentValues);
+            sqLiteDatabase.setTransactionSuccessful();//当所有操作结束后调用，通过setTransactionSuccessful将数据保存在数据库里
+
+        }catch (Exception e){
+            Log.e(CH14Activity1.class.toString(),e.toString());
+        }finally {
+            sqLiteDatabase.endTransaction();//结束事务
+            //使用完毕，将数据库关闭
+            sqLiteDatabase.close();
+        }
+    }
+    public void query(View view){
+        SQLiteDatabase sqLiteDatabase=myOpenHelper.getWritableDatabase();
+        try{
+            Cursor cursor=sqLiteDatabase.rawQuery("select * from student where stuname=?",new String["Tom"]);//原生的SQL语句
+            while (cursor.moveToNext()){
+                int id=cursor.getInt(cursor.getColumnIndex("id"));
+                String stuname=cursor.getString(cursor.getColumnIndex("stuname"));
+                String stutel=cursor.getString(cursor.getColumnIndex("stutel"));
+                Log.i(CH14Activity1.class.toString(),"id:"+id+",stuname:"+stuname+",stutel:"+stutel);
+            }
+        }catch (Exception e){
+            Log.e(CH14Activity1.class.toString(),e.toString());
+        }finally {
+
+            //使用完毕，将数据库关闭
+            sqLiteDatabase.close();
+        }
+    }
+    public void delete(View view){
+        //以可写方式打开数据库(如果数据库不存在，自动创建数据库)
+        SQLiteDatabase sqLiteDatabase=myOpenHelper.getWritableDatabase();
+        try{
+            //将插入的数据放置在ContentValues中
+            //事务的处理
+            sqLiteDatabase.beginTransaction();//开启事务
+
+            sqLiteDatabase.delete("student","id=?",new String[]{"1"});
+            sqLiteDatabase.setTransactionSuccessful();//当所有操作结束后调用，通过setTransactionSuccessful将数据保存在数据库里
+
+        }catch (Exception e){
+            Log.e(CH14Activity1.class.toString(),e.toString());
+        }finally {
+            sqLiteDatabase.endTransaction();//结束事务
+            //使用完毕，将数据库关闭
+            sqLiteDatabase.close();
+        }
+    }
+    public void edit(View view){
+        //以可写方式打开数据库(如果数据库不存在，自动创建数据库)
+        SQLiteDatabase sqLiteDatabase=myOpenHelper.getWritableDatabase();
+        try{
+            //将插入的数据放置在ContentValues中
+            //事务的处理
+            sqLiteDatabase.beginTransaction();//开启事务
+
+            ContentValues contentValues=new ContentValues();
+            contentValues.put("stutel","15999999999");
+
+            sqLiteDatabase.update("student",contentValues,"id=?",new String[]{"2"});
+            sqLiteDatabase.setTransactionSuccessful();//当所有操作结束后调用，通过setTransactionSuccessful将数据保存在数据库里
+
+        }catch (Exception e){
+            Log.e(CH14Activity1.class.toString(),e.toString());
+        }finally {
+            sqLiteDatabase.endTransaction();//结束事务
+            //使用完毕，将数据库关闭
+            sqLiteDatabase.close();
+        }
+    }
+}
---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180230/MyOpenHelper.java
@@ -0,0 +1,33 @@
+package cn.edu.sdwu.android02.classroom.sn170507180230;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+/**
+ * Created by dell on 2020/4/29.
+ */
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+    private String STUDENT_TB_SQL="create table student(id integer primary key autoincrement,stuname text,stutel text)";
+    public MyOpenHelper(Context context){
+        //Context context上下文，String name数据库名称，CursorFactory factory传入NULL，int version 数据库的版本
+        super(context,"stud.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //当打开数据库时，数据库并不存在，此时会自动创建数据库，然后调用onCreate方法
+        //在本方法，完成数据库对象（表，视图，索引等）的创建
+        //通过sqLiteDatabase对象，执行SQL
+        sqLiteDatabase.execSQL(STUDENT_TB_SQL);
+        Log.i(MyOpenHelper.class.toString(),"onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
+
